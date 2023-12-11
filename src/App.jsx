4ae18b534d5d1385ae09558970e3a8d890f74af9
Tanveerxz <tanveersingh252008@gmail.com>
2023-12-12 00:49:35 +0530

    v1.0.0 public release

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useSelector } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+
+import { Searchbar, Sidebar, MusicPlayer, TopPlay } from './components';
+import { ArtistDetails, TopArtists, AroundYou, Discover, Search, SongDetails, TopCharts } from './pages';
+
+import './index.css'
+
+const App = () => {
+  const { activeSong } = useSelector((state) => state.player);
+
+  return (
+    <div className="bg">
+    <div className="relative flex select-none">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Searchbar />
+
+        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse select-none">
+          <div className="flex-1 h-fit pb-40">
+            <Routes>
+              <Route path="/" element={<Discover />} />
+              <Route path="/top-artists" element={<TopArtists />} />
+              <Route path="/top-charts" element={<TopCharts />} />
+              <Route path="/around-you" element={<AroundYou />} />
+              <Route path="/artists/:id" element={<ArtistDetails />} />
+              <Route path="/songs/:songid" element={<SongDetails />} />
+              <Route path="/search/:searchTerm" element={<Search />} />
+            </Routes>
+          </div>
+          <div className="xl:sticky relative top-0 h-fit">
+            <TopPlay />
+          </div>
+        </div>
+      </div>
+
+      {activeSong?.title && (
+        <div className="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-t-3xl z-10">
+          <MusicPlayer />
+        </div>
+      )}
+    </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/assets/index.js
@@ -0,0 +1,9 @@
+import loader from './loader.svg';
+import logo from './logo.svg';
+import logo2 from './logo2.png';
+
+export {
+  logo,
+  loader,
+  logo2,
+};
